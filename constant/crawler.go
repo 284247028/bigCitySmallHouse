17e2e97d4a349512e0d59962ce239ec116758b68
qiu <707904641@qq.com>
2022-08-28 20:51:47 +0800

    [删除无用目录]

--- constant/crawler.go
+++
@@ -1,6 +0,0 @@
-package constant
-
-const (
-	CrawlerTypeSingle = "single"
-	CrawlerTypeList   = "list"
-)
--- structure/param/crawler.go
+++
@@ -1,7 +0,0 @@
-package param
-
-type CrawlerParam struct {
-	Type string //	爬取类型，单个-列表
-	Url  string // 指定房源详情页
-	Page int    // 爬取列表时，第几页
-}
