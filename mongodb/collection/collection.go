f62064aeb358512404c2ae6c272f68b33acc0bac
qiu <707904641@qq.com>
2022-09-19 22:57:00 +0800

    [mongodb] crud

--- mongodb/collection/collection.go
+++ mongodb/collection/collection.go
@@ -2,7 +2,10 @@ package collection
 
 import (
 	"bigCitySmallHouse/mongodb"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Options struct {
@@ -27,4 +30,12 @@ func NewCollection(opts *Options) *Collection {
 
 func (receiver *Collection) MCollection() *mongo.Collection {
 	return receiver.mCollection
+}
+
+func (receiver *Collection) UpsertMany(filter, object interface{}, opts *options.UpdateOptions) (*mongo.UpdateResult, error) {
+	update := bson.D{
+		{"$set", object},
+	}
+	opts.SetUpsert(true)
+	return receiver.mCollection.UpdateOne(context.TODO(), filter, update, opts)
 }
---
+++ mongodb/tests/mongodb_test.go
@@ -0,0 +1,55 @@
+package tests
+
+import (
+	"bigCitySmallHouse/model/house"
+	"bigCitySmallHouse/mongodb"
+	"bigCitySmallHouse/mongodb/collection"
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"log"
+	"testing"
+)
+
+func TestUpsert(t *testing.T) {
+	err := mongodb.NewDB().ConnectMongodb(mongodb.NewOptions())
+	if err != nil {
+		t.Fatal(err)
+	}
+	coll := collection.NewCollection(collection.NewOptions())
+
+	houses := make([]house.House, 0)
+	houses = append(houses, house.House{
+		SourceId: "111222333",
+	})
+
+	houses = append(houses, house.House{
+		SourceId: "222333444",
+	})
+
+	for _, h := range houses {
+
+		marshal, err := bson.Marshal(h)
+		if err != nil {
+			t.Fatal(err)
+			return
+		}
+
+		m := bson.M{}
+		err = bson.Unmarshal(marshal, &m)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		update := bson.M{
+			"$set": h,
+		}
+		//opts := &options.ReplaceOptions{}
+		//opts.SetUpsert(true)
+		replaceOne, err := coll.MCollection().UpdateOne(context.TODO(), bson.D{{"source_id", h.SourceId}}, update)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(replaceOne)
+	}
+	log.Println("end")
+}
